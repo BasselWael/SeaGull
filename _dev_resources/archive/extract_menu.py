9f3cd3e39fb0c9e740da805c2cd1bfea270abbaf
Bassel <basel.wael.h@gmail.com>
2026-07-07 17:03:35 +0300

    Final

--- _dev_resources/archive/extract_menu.py
+++
@@ -1,6 +0,0 @@
-import re
-
-text = open('el_max.txt', 'r').read()
-
-# Categories
-# I will just write a small hardcoded JSON based on the text file.
--- _dev_resources/archive/fix_script.py
+++
@@ -1,9 +0,0 @@
-with open("script.js", "r") as f:
-    text = f.read()
-
-# Replace the trailing mess
-text = text.replace("    });\n\n    });\n\n});", "});")
-text = text.replace("    });\n\n});", "});")
-
-with open("script.js", "w") as f:
-    f.write(text)
